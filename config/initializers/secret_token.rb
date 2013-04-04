585e4071ebe0832549109b91e6ded6c077f69817
Paul Benigeri <benigeri@stanford.edu>
2013-04-03 23:49:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigablab::Application.config.secret_token = '695c77e877206c13c5c575e9c2708c1374f9237ffeefd39f744e5236f9495c7ad668aee0e279ea5197cbd95cab67672d79f2c436d726c9223abf66d3db6af472'
