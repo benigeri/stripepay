bdd01f07c7cf135b9dce6b4fb9ec66113632eeed
Paul Benigeri <benigeri@stanford.edu>
2013-04-04 00:56:37 -0700

    add basic nav, layout, simple form, base errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def display_base_errors resource
+    return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
+    messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-block">
+      #{messages}
+    </div>
+    HTML
+    html.html_safe
+  end
+
 end
