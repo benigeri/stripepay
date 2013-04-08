4dee72b137bd075d1be763a2a9007dca6f538852
Paul Benigeri <benigeri@stanford.edu>
2013-04-07 22:05:01 -0700

    add working stripe subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,55 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :role_ids, :as => :admin
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :stripe_token, :coupon
 
   validates_presence_of :first_name, :last_name
   validates_uniqueness_of :email, :case_sensitive => false
 
-  # attr_accessible :title, :body
+  attr_accessor :stripe_token, :coupon
+
+  before_save :update_stripe
+  
+  def update_stripe
+    return if email.include?(ENV['ADMIN_EMAIL'])
+    return if email.include?('@example.com') and not Rails.env.production?
+    if customer_id.nil?
+      if !stripe_token.present?
+        raise "Stripe token not present. Can't create account."
+      end
+      if coupon.blank?
+        customer = Stripe::Customer.create(
+          :email => email,
+          :description => first_name + " " + last_name,
+          :card => stripe_token,
+          :plan => roles.first.name
+        )
+      else
+        customer = Stripe::Customer.create(
+          :email => email,
+          :description => first_name + " " + last_name,
+          :card => stripe_token,
+          :plan => roles.first.name,
+          :coupon => coupon
+        )
+      end
+    else
+      customer = Stripe::Customer.retrieve(customer_id)
+      if stripe_token.present?
+        customer.card = stripe_token
+      end
+      customer.email = email
+      customer.description = first_name + " " + last_name
+      customer.save
+    end
+    self.last_4_digits = customer.active_card.last4
+    self.customer_id = customer.id
+    self.stripe_token = nil
+  rescue Stripe::StripeError => e
+    logger.error "Stripe Error: " + e.message
+    errors.add :base, "#{e.message}."
+    self.stripe_token = nil
+    false
+  end
+
 end
