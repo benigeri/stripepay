448760904b667e7dec6b44fcca8cae3a931bb536
Paul Benigeri <benigeri@stanford.edu>
2013-04-07 22:38:37 -0700

    fix bug when saving new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
     return if email.include?(ENV['ADMIN_EMAIL'])
     return if email.include?('@example.com') and not Rails.env.production?
     if customer_id.nil?
-      if stripe_token.present?
+      if !stripe_token.present?
         raise "Stripe token not present. Can't create account."
       end
       if coupon.blank?
